1c46848835ccb5b8d5f6fd710d15b6b7c2f986b7
DarcoProgramador <denangel123@gmail.com>
2024-05-30 12:47:55 -0600

    fix errors in delete player

--- internal/repositories/mongo/player/delete.go
+++ internal/repositories/mongo/player/delete.go
@@ -7,27 +7,35 @@ import (
 
 	"github.com/jairogloz/go-l/internal/domain"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var (
-	errPlayerNotFound = errors.New("player not found")
-	errDeletePlayer   = errors.New("error deleting player")
+	ErrPlayerNotFound = errors.New("player not found")
+	ErrDeletePlayer   = errors.New("error deleting player")
+	ErrIncorrectHexID = errors.New("incorrect mongo Hex format")
 )
 
 // Used for delete one player by id from the database
 func (r *Repository) Delete(id domain.Player) (err error) {
 
+	playerID, err := primitive.ObjectIDFromHex(id.ID)
+	if err != nil {
+		slog.Error("converting id to object id: ", slog.Any("mongodb", err))
+		return ErrIncorrectHexID
+	}
+
 	collection := r.Client.Database("go-l").Collection("players")
-	deleteResult, err := collection.DeleteOne(context.TODO(), bson.M{"_id": id})
+	deleteResult, err := collection.DeleteOne(context.TODO(), bson.M{"_id": playerID})
 
 	if err != nil {
 		slog.Error("deleting player: ", slog.Any("mongodb", err))
-		return errDeletePlayer
+		return ErrDeletePlayer
 	}
 
 	if deleteResult.DeletedCount == 0 {
 		slog.Error("player not found")
-		return errPlayerNotFound
+		return ErrPlayerNotFound
 	}
 
 	return nil
