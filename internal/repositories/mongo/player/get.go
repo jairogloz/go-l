b3113aba097e428252c7dc096b1fa4fb7b98b318
ignacior <ignacioruedaboada@hotmail.com>
2024-05-30 10:36:44 -0500

    update get player repository

--- internal/repositories/mongo/player/get.go
+++ internal/repositories/mongo/player/get.go
@@ -1,8 +1,38 @@
 package player
 
-import "github.com/jairogloz/go-l/internal/domain"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/jairogloz/go-l/internal/domain"
+)
+
+type GetPlayerError struct {
+	Err error
+}
+
+func (e GetPlayerError) Error() string {
+	return fmt.Sprintf("error getting player: %s", e.Err.Error())
+}
 
 func (r *Repository) Get(id domain.Player) (player domain.Player, err error) {
-	//TODO implement me
-	panic("implement me")
+	playerID, err := primitive.ObjectIDFromHex(id.ID)
+	if err != nil {
+		log.Println(err.Error())
+		err = GetPlayerError{Err: err}
+		return
+	}
+
+	collection := r.Client.Database("go-l").Collection("players")
+	err = collection.FindOne(context.Background(), bson.M{"_id": playerID}).Decode(&player)
+	if err != nil {
+		log.Println(err.Error())
+		err = GetPlayerError{Err: err}
+		return
+	}
+	return
 }
