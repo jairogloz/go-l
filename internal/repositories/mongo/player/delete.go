3ed167ff3c292ad4ae283f6dc1c927baa1b7854d
DarcoProgramador <denangel123@gmail.com>
2024-05-29 23:12:44 -0600

    update delete player repositories
    - add funtionality to delete player by id

--- internal/repositories/mongo/player/delete.go
+++ internal/repositories/mongo/player/delete.go
@@ -1,8 +1,34 @@
 package player
 
-import "github.com/jairogloz/go-l/internal/domain"
+import (
+	"context"
+	"errors"
+	"log/slog"
 
+	"github.com/jairogloz/go-l/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var (
+	errPlayerNotFound = errors.New("player not found")
+	errDeletePlayer   = errors.New("error deleting player")
+)
+
+// Used for delete one player by id from the database
 func (r *Repository) Delete(id domain.Player) (err error) {
-	//TODO implement me
-	panic("implement me")
+
+	collection := r.Client.Database("go-l").Collection("players")
+	deleteResult, err := collection.DeleteOne(context.TODO(), bson.M{"_id": id})
+
+	if err != nil {
+		slog.Error("deleting player: ", slog.Any("mongodb", err))
+		return errDeletePlayer
+	}
+
+	if deleteResult.DeletedCount == 0 {
+		slog.Error("player not found")
+		return errPlayerNotFound
+	}
+
+	return nil
 }
