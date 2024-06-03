de880c560fef622b104f6a7d29b02563825204b3
DarcoProgramador <denangel123@gmail.com>
2024-06-03 13:04:29 -0600

    Implement delete player service functionality
    - Actualizando el puerto del servicio para eliminar un jugador
    - Añadiendo funcionalidad al servicio para eliminar un jugador
    - Añadiendo nuevos errores de dominio
    - Actulizando el repositorio para eliminar un jugador para usar los errores de dominio

--- internal/ports/player.go
+++ internal/ports/player.go
@@ -5,7 +5,7 @@ import "github.com/jairogloz/go-l/internal/domain"
 type PlayerService interface {
 	Create(player *domain.Player) (err error)
 	Get(id domain.Player) (player domain.Player, err error)
-	Delete(id domain.Player) (err error)
+	Delete(id string) (err error)
 }
 
 type PlayerRepository interface {
