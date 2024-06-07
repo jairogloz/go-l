4b969d579bf277fcfa1933d643cb8eefd0ea3efb
simpleReno <ult.ofr.tato@gmail.com>
2024-06-06 19:56:29 -0600

    Add Errors to handle Repo errors

--- internal/repositories/mongo/team/delete.go
+++ internal/repositories/mongo/team/delete.go
@@ -1,6 +1,15 @@
 package team
 
-import "context"
+import (
+	"context"
+	"errors"
+)
+
+var (
+	ErrTeamNotFound   = errors.New("player not found")
+	ErrDeleteTeam     = errors.New("error deleting player")
+	ErrIncorrectHexID = errors.New("incorrect mongo Hex format")
+)
 
 func (r Repository) Delete(ctx context.Context, id string) (err error) {
 	//TODO implement me
