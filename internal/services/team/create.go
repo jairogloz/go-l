3707557246ec567779ba0327f07e3acb4d0174d7
simpleReno <ult.ofr.tato@gmail.com>
2024-06-07 23:07:03 -0600

    Implement Create team service

--- internal/services/team/create.go
+++ internal/services/team/create.go
@@ -2,10 +2,24 @@ package team
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/jairogloz/go-l/internal/domain"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (s Service) Create(ctx context.Context, team *domain.Team) (err error) {
-	//TODO implement me
-	panic("implement me")
+	now := time.Now().UTC()
+	team.CreatedAt = primitive.DateTime(now.UnixNano() / int64(time.Millisecond))
+
+	err = s.Repo.Insert(ctx, team)
+	if err != nil {
+		appErr := domain.ManageError(err, "")
+		log.Println(appErr.Error())
+		return fmt.Errorf("error creating team: %w", appErr)
+	}
+
+	return nil
 }
