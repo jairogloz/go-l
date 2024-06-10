f43d7797ec1706f0504d3c27341857be2562497f
David matias corbalan <dmcorbalan@teco.com.ar>
2024-06-10 18:23:21 -0300

    implement get team by id for service get

--- internal/services/team/get.go
+++ internal/services/team/get.go
@@ -2,10 +2,28 @@ package team
 
 import (
 	"context"
+	"fmt"
+	"log"
+
 	"github.com/jairogloz/go-l/internal/domain"
 )
 
+type GetTeamError struct {
+	details error
+}
+
+func (e GetTeamError) Error() string {
+	return fmt.Sprintf("error getting team: %s", e.details.Error())
+}
+
 func (s Service) Get(ctx context.Context, id string) (team domain.Team, players []domain.Player, err error) {
-	//TODO implement me
-	panic("implement me")
+
+	team, err = s.Repo.Get(ctx, id)
+	if err != nil {
+		log.Println(err.Error())
+		err = GetTeamError{err}
+		return
+	}
+
+	return
 }
