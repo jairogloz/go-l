d4db8c899e21f45ee594012da5f9eddd2a60e056
simpleReno <ult.ofr.tato@gmail.com>
2024-06-13 14:40:42 -0600

    implement delete team service

--- internal/services/team/delete.go
+++ internal/services/team/delete.go
@@ -7,6 +7,17 @@ import (
 	"github.com/jairogloz/go-l/internal/domain"
 )
 
+/*
+Delete a team by id
+
+Params:
+- ctx: context.Context
+- id: string
+
+Return:
+- error
+*/
+
 func (s Service) Delete(ctx context.Context, id string) (err error) {
 
 	err = s.Repo.Delete(ctx, id)
