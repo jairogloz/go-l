fab3f004d5e1ab2900de36b2a737ff11db27edab
Julián Ruiz <jnrt96@gmail.com>
2024-06-16 00:41:32 -0700

    add a repository function to handle removal of a team by id

--- internal/repositories/mongo/team/delete.go
+++ internal/repositories/mongo/team/delete.go
@@ -1,8 +1,39 @@
 package team
 
-import "context"
+import (
+	"context"
+	"errors"
+	"log/slog"
 
+	"github.com/jairogloz/go-l/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var (
+	ErrDeleteTeam = errors.New("error deleting team")
+)
+
+// deletes one team by id from db
 func (r Repository) Delete(ctx context.Context, id string) (err error) {
-	//TODO implement me
-	panic("implement me")
+
+	teamID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		slog.Error("converting id to object id: ", slog.Any("mongodb", err))
+		return domain.ErrIncorrectID
+	}
+
+	collection := r.Client.Database("go-l").Collection("teams")
+	deleteResult, err := collection.DeleteOne(context.TODO(), bson.M{"_id": teamID})
+	if err != nil {
+		slog.Error("deleting team: ", slog.Any("mongodb", err))
+		return ErrDeleteTeam
+	}
+
+	if deleteResult.DeletedCount == 0 {
+		slog.Error("player not found")
+		return domain.ErrNotFound
+	}
+
+	return nil
 }
