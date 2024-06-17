8920978ad9468081c622586c99628b0c7bc318bc
Sebast00 <sebastian.torres@getlenk.com>
2024-06-17 09:50:50 -0300

    implement get team by id repository

--- internal/repositories/mongo/team/get.go
+++ internal/repositories/mongo/team/get.go
@@ -2,10 +2,27 @@ package team
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"github.com/jairogloz/go-l/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (r Repository) Get(ctx context.Context, id string) (team domain.Team, err error) {
-	//TODO implement me
-	panic("implement me")
+	teamID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Println(err.Error())
+		return team, fmt.Errorf("%w: error getting team: %s", domain.ErrIncorrectID, err)
+	}
+
+	collection := r.Client.Database("go-l").Collection("teams")
+	err = collection.FindOne(ctx, bson.M{"_id": teamID}).Decode(&team)
+	if err != nil {
+		log.Println(err.Error())
+		return team, fmt.Errorf("%w: error getting team: %s", domain.ErrNotFound, err)
+	}
+
+	team.ID = teamID.Hex()
+	return
 }
