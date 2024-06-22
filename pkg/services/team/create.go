97887e545ff256e250edbdb2c6c2002677c96db6
simpleReno <ult.ofr.tato@gmail.com>
2024-06-22 15:18:22 -0600

    implement create team service

--- pkg/services/team/create.go
+++ pkg/services/team/create.go
@@ -2,10 +2,14 @@ package team
 
 import (
 	"context"
+
 	"github.com/jairogloz/go-l/pkg/domain"
 )
 
 func (s Service) Create(ctx context.Context, team *domain.Team) (err error) {
-	//TODO implement me
-	panic("implement me")
+	err = s.Repo.Insert(ctx, team)
+	if err != nil {
+		return domain.ManageError(err, "Error creating team")
+	}
+	return nil
 }
