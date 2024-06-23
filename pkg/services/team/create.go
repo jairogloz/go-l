f3d19df9456b88c862eaf8a3cb312847c4a6ebfb
simpleReno <ult.ofr.tato@gmail.com>
2024-06-23 13:56:57 -0600

    comment

--- pkg/services/team/create.go
+++ pkg/services/team/create.go
@@ -6,6 +6,13 @@ import (
 	"github.com/jairogloz/go-l/pkg/domain"
 )
 
+/*
+Summary: Create a new team based on the provided data and store it in the database
+Method: Create
+Params: context.Context, *domain.Team
+Return: error
+*/
+
 func (s Service) Create(ctx context.Context, team *domain.Team) (err error) {
 	err = s.Repo.Insert(ctx, team)
 	if err != nil {
