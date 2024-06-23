ba3e5e98846940637ab8691e2f5bfe98484ac192
Julián Ruiz <jnrt96@gmail.com>
2024-06-23 15:14:58 -0700

    delete logs on delete repository team method

--- pkg/repositories/mongo/team/delete.go
+++ pkg/repositories/mongo/team/delete.go
@@ -3,7 +3,6 @@ package team
 import (
 	"context"
 	"fmt"
-	"log/slog"
 
 	"github.com/jairogloz/go-l/pkg/domain"
 	"go.mongodb.org/mongo-driver/bson"
@@ -15,18 +14,15 @@ func (r Repository) Delete(ctx context.Context, id string) (err error) {
 
 	teamID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		slog.Error("converting id to object id: ", slog.Any("mongodb", err))
 		return domain.ErrIncorrectID
 	}
 
 	deleteResult, err := r.Collection.DeleteOne(ctx, bson.M{"_id": teamID})
 	if err != nil {
-		slog.Error("deleting team: ", slog.Any("mongodb", err))
 		return fmt.Errorf("error deleting team: %s", err.Error())
 	}
 
 	if deleteResult.DeletedCount == 0 {
-		slog.Error("player not found")
 		return domain.ErrNotFound
 	}
 
