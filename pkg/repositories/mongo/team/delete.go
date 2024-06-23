d2f09658ec8a256d14d87cde73b268c4be167afb
Julián Ruiz <jnrt96@gmail.com>
2024-06-23 14:51:19 -0700

    change comment on delete repository team method, use ctx argument

--- pkg/repositories/mongo/team/delete.go
+++ pkg/repositories/mongo/team/delete.go
@@ -2,14 +2,15 @@ package team
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 
-	"github.com/jairogloz/go-l/internal/domain"
+	"github.com/jairogloz/go-l/pkg/domain"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// deletes one team by id from db
+// Delete deletes a team by id from db
 func (r Repository) Delete(ctx context.Context, id string) (err error) {
 
 	teamID, err := primitive.ObjectIDFromHex(id)
@@ -18,10 +19,10 @@ func (r Repository) Delete(ctx context.Context, id string) (err error) {
 		return domain.ErrIncorrectID
 	}
 
-	deleteResult, err := r.Collection.DeleteOne(context.TODO(), bson.M{"_id": teamID})
+	deleteResult, err := r.Collection.DeleteOne(ctx, bson.M{"_id": teamID})
 	if err != nil {
 		slog.Error("deleting team: ", slog.Any("mongodb", err))
-		return domain.ErrDeleteTeam
+		return fmt.Errorf("error deleting team: %s", err.Error())
 	}
 
 	if deleteResult.DeletedCount == 0 {
