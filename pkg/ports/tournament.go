4f15088056277f1401f6ab00a412f629d1af7691
ignacior <ignacioruedaboada@hotmail.com>
2024-07-07 15:40:17 -0500

    Add delete tournament ports

--- pkg/ports/tournament.go
+++ pkg/ports/tournament.go
@@ -9,9 +9,11 @@ import (
 // TournamentService is the interface that have methods to interact with the tournament entity.
 type TournamentService interface {
 	Create(ctx context.Context, tournament *domain.Tournament) (err error)
+	Delete(ctx context.Context, id string) (err error)
 }
 
 // TournamentRepository is the interface that have methods to interact with the tournament entity in the database.
 type TournamentRepository interface {
 	Insert(ctx context.Context, tournament *domain.Tournament) (err error)
+	Delete(ctx context.Context, id string) (err error)
 }
