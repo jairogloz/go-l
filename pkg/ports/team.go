d30d0d19a64bfdf8587084419a0aac5960e2edea
DarcoProgramador <denangel123@gmail.com>
2024-07-14 14:31:04 -0600

    add team get service and mock

--- pkg/ports/team.go
+++ pkg/ports/team.go
@@ -2,12 +2,13 @@ package ports
 
 import (
 	"context"
+
 	"github.com/jairogloz/go-l/pkg/domain"
 )
 
 type TeamService interface {
 	Create(ctx context.Context, team *domain.Team) (err error)
-	Get(ctx context.Context, id string) (team domain.Team, players []domain.Player, err error)
+	Get(ctx context.Context, id string) (team *domain.Team, players []domain.Player, err error)
 	Delete(ctx context.Context, id string) (err error)
 }
 
--- pkg/services/team/get.go
+++ pkg/services/team/get.go
@@ -2,10 +2,22 @@ package team
 
 import (
 	"context"
+
 	"github.com/jairogloz/go-l/pkg/domain"
 )
 
-func (s Service) Get(ctx context.Context, id string) (team domain.Team, players []domain.Player, err error) {
-	//TODO implement me
-	panic("implement me")
+// Get retrieves a team and its players.
+func (s Service) Get(ctx context.Context, id string) (team *domain.Team, players []domain.Player, err error) {
+	team, err = s.Repo.Get(ctx, id)
+
+	if err != nil {
+		return nil, nil, domain.ManageError(err, "unexpected error getting team")
+	}
+
+	players, err = s.Repo.GetPlayers(ctx, team.ID.(string))
+	if err != nil {
+		return nil, nil, domain.ManageError(err, "unexpected error getting players")
+	}
+
+	return team, players, nil
 }
