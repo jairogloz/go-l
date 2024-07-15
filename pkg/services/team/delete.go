b6f3353d2031ea31c7ad5b3e50dcc1bedee5284b
DarcoProgramador <denangel123@gmail.com>
2024-07-14 19:57:03 -0600

    update delete team service for add comments

--- pkg/services/team/delete.go
+++ pkg/services/team/delete.go
@@ -6,6 +6,8 @@ import (
 	"github.com/jairogloz/go-l/pkg/domain"
 )
 
+// Delete removes a team from the repository.
+// It returns an error if the team does not exist or if there is an unexpected error.
 func (s Service) Delete(ctx context.Context, id string) (err error) {
 
 	err = s.Repo.Delete(ctx, id)
