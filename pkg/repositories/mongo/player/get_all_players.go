700901fbe2317a7a34d575c4c7ca9b4a8a733a23
Andres Zapata <afzar27@gmail.com>
2024-09-02 22:46:14 -0500

    fix: error adjustment for correct structs
    - Adjusted error handling in repository methods.
    - Corrected struct definitions and usage in repository and service layers.

--- pkg/repositories/mongo/player/get_all_players.go
+++ pkg/repositories/mongo/player/get_all_players.go
@@ -5,14 +5,21 @@ import (
 	"fmt"
 	"github.com/jairogloz/go-l/pkg/domain"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// GetAll retrieves all players from the MongoDB collection.
 func (r *Repository) GetAll() ([]*domain.Player, error) {
 	cursor, err := r.Collection.Find(context.Background(), bson.M{})
 	if err != nil {
 		return nil, fmt.Errorf("error getting players: %w", err)
 	}
-	defer cursor.Close(context.Background())
+	defer func(cursor *mongo.Cursor, ctx context.Context) {
+		err := cursor.Close(ctx)
+		if err != nil {
+			fmt.Printf("error closing cursor: %v", err)
+		}
+	}(cursor, context.Background())
 
 	var players []*domain.Player
 	for cursor.Next(context.Background()) {
@@ -22,9 +29,6 @@ func (r *Repository) GetAll() ([]*domain.Player, error) {
 			return nil, fmt.Errorf("error decoding player: %w", err)
 		}
 		players = append(players, &player)
-	}
-	if len(players) == 0 {
-		return nil, domain.ErrNotFound
 	}
 
 	return players, nil
--- pkg/services/player/get_all.go
+++ pkg/services/player/get_all.go
@@ -1,12 +1,28 @@
 package player
 
-import "github.com/jairogloz/go-l/pkg/domain"
+import (
+	"errors"
+	"fmt"
+	"github.com/jairogloz/go-l/pkg/domain"
+	"log"
+)
 
+// Get retrieves a player by their ID from the repository.
+// It returns an Array of domain.Player objects and an error.
+// If the player is not found in the repository, it returns a domain-specific not found error.
+// If there is a timeout error when accessing the repository, it returns a domain-specific timeout error.
+// For any other errors, it logs the error and returns a generic error.
 func (s *Service) GetAll() (player []*domain.Player, err error) {
 
 	player, err = s.Repo.GetAll()
 	if err != nil {
-		return nil, err
+		if errors.Is(err, domain.ErrTimeout) {
+			return nil, domain.NewAppError(
+				domain.ErrCodeTimeout,
+				"timeout error, try again later")
+		}
+		log.Println(err.Error())
+		return nil, fmt.Errorf("unexpected error getting players: %w", err)
 	}
 
 	return player, nil
